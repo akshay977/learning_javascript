ba5f9fc6ac6e0fe62a3710de012b3f5bec01e04d
Akshay Chandrakanth <akshay.chandrakanth@vedak.com>
2024-06-25 22:24:31 +0530

    Datatypes in javascript started + interview questions

---
+++ Datatypes.js
@@ -0,0 +1,19 @@
+/*
+	1. A value in javascript can be of 2 types:
+		a. Object: let me = { name: 'Akshay' }
+		b. Primitive: let salary = 18999;
+	
+	2. There are 7 primitive datatypes in Javascript:
+		a. Number: Floating point numbers, Used for decimals and integers Eg. let age = 23
+		b. String: Sequence of characters, Used for text Eg. let firstName = 'Jonas'
+		c. Boolean: Logical type that can only be true or false, Used for taking decisions Eg. let isEdit = true
+		d. Undefined: Value taken by a variable that is not yet defined. Eg. let children;
+		e. Null: Also means 'empty value'
+		f. Symbol (ES2015): Value that is unique and cannot be changed.
+		g. Bigint (ES2020): Larger integers than the Number type can hold
+
+	3. Dynamic Typing:
+		In dynamic typing data types are determined automatically by javascript and we do not have to manually define the
+		datatype of the value stored in the variable. In javascript, value has a datatype and not the variable so 
+		a new value with different datatype can be stored in the variable.
+*/
