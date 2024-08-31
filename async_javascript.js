129eea59522bfc93e8681a92aa64228006939e74
Akshay Chandrakanth <akshay.chandrakanth@vedak.com>
2024-08-31 16:12:36 +0530

    testing 2

--- async_javascript.js
+++ async_javascript.js
@@ -25,7 +25,6 @@
 	https://restcountries.com/v3.1/name/deutschland
 */
 
-
 // XMLHttpRequest: It is the most old school way of sending API requests
 
 // Following Request code works in browser only
