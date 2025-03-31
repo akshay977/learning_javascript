1d6502be9fa5a12976c089833f239c3ad8267d7d
AkshayC977 <akshayjoshi977@gmail.com>
2025-03-31 16:29:00 +0530

    interface seggregation principle

---
+++ design_patterns/04_interface_seggregation_principle.js
@@ -0,0 +1,71 @@
+class Machine {
+	constructor() {
+		if (this.constructor.name === 'Machine') 
+			throw new Error('Machine is abstract');
+	}
+
+	print(doc) {}
+	scan(doc) {}
+	fax(doc) {}
+}
+
+class MultiFunctionPrinter extends Machine {
+	print(doc) {
+		// do something
+	}
+
+	scan(doc) {
+		// do something
+	}
+
+	fax(doc) {
+		// do something
+	}
+}
+
+/*
+	OldFashionedPrinters cannot scan or fax so those
+	methods need not be implemented and cannot be left blank
+	or no errors should be thrown as that is bad approach.
+	So, here comes the interface seggregation principle.
+*/
+class OldFashionedPrinter extends Machine {
+	print(doc) {
+		// do something
+	}
+}
+
+
+/*
+	Machine class (abstract interface) is split up like
+	below
+*/
+
+class Printer {
+	constructor() {
+		if (this.constructor.name === 'Printer') 
+			throw new Error('Printer is abstract');
+	}
+
+	print(doc) {}
+}
+
+class Scanner {
+	constructor() {
+		if (this.constructor.name === 'Scanner') 
+			throw new Error('Scanner is abstract');
+	}
+
+	scan(doc) {}
+}
+
+/*
+	In the case of photocopier multiple inheritances
+	are not possible so PhotoCopier class contains
+	methods print and scan without inheritance
+*/
+class PhotoCopier {
+	print() {}
+	scan() {}
+}
+
